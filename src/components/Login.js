16b060e974eb993c71d40373c7e13f82ef1d4969
irmo12 <omriws@hushmail.com>
2022-11-14 17:59:49 +0200

    style login/register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
-import { Link, withRouter } from 'react-router-dom';
-import api from "../utils/api";
+import { Link } from 'react-router-dom';
+// import api from "../utils/api";
 
 function Login({handleSubmit}) {
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
- { 
-    return (
-      <div className="login">
-        <h1 className="login__welcome">
-          Welcome back!
-        </h1>
-        <form onSubmit={handleSubmit} className="login__form">
-          <input required id="email" name="email" type="text" value={email} onChange={e=>setEmail(e.target.value)} />
-          <input required id="password" name="password" type="password" value={password} onChange={e=>setPassword(e.target.value)} />
-          <div className="login__button-container">
-            <button type="submit" className="login__link">Log in</button>
-          </div>
-        </form>
-        <div className="login__signup">
-          <Link to="/register" className="signup__link">Not a member yet? Sign up here!</Link>
-        </div>
+  
+  return (
+    <div className="auth">
+      <h1 className="auth__heading">
+        Log in
+      </h1>
+      <form onSubmit={handleSubmit} className="auth__form">
+      <div className="auth__inputs">
+        <input className="auth__input" required id="email" name="email" type="text" value={email} onChange={e=>setEmail(e.target.value)} />
+        <input className="auth__input" required id="password" name="password" type="password" value={password} onChange={e=>setPassword(e.target.value)} />
       </div>
+      <div className="auth__button-container">
+        <button type="submit" className="auth__submit">Log in</button>
+      </div>
+      </form>
+      <div className="auth__signup">
+        <Link className="auth__link" to="/register">Not a member yet? Sign up here!</Link>
+      </div>
+    </div>
     );
-  }
 }
 
 export default Login;
